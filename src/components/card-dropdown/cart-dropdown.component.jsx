a8abf098fb2410a06b66c77e27ec61785b747cf6
vinay.n <vinay.n@tavant.com>
2020-05-03 23:40:36 +0530

    added redux for cart component

---
+++ src/components/card-dropdown/cart-dropdown.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import CustomButton from '../custom-button/custom-button.component'
+import './cart-dropdown.styles.scss';
+
+const CartDropDown = () => (
+    <div className="cart-dropdown">
+        <div className="cart-items"></div>
+        <CustomButton>Go To Checkout</CustomButton>
+    </div>
+);
+
+export default CartDropDown;
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -4,8 +4,10 @@ import './header.styles.scss';
 import { ReactComponent as Logo } from '../../assets/images/crwn.svg';
 import { auth } from '../../firebase/firebase.util';
 import { connect } from 'react-redux';
+import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropDown from '../card-dropdown/cart-dropdown.component';
 
-export const Header = ({ currentUser }) => (
+export const Header = ({ currentUser, hidden }) => (
     <div className="header">
         <Link to="/" className="logo-container" >
             <Logo className="logo" />
@@ -19,12 +21,17 @@ export const Header = ({ currentUser }) => (
                     :
                     <Link className="option" to="/signin">SIGN IN</Link>
             }
+            <CartIcon />
         </div>
+        {
+            !hidden ? <CartDropDown /> : null
+        }
     </div>
 )
 
-const mapStateToProps = state => ({
-    currentUser: state.user.currentUser
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
+    currentUser,
+    hidden
 })
 
 export default connect(mapStateToProps)(Header);
